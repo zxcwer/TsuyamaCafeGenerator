5e6469ca5b7b8fc4a87ac594a7c7f6247f436480
zxcwer <61655281+zxcwer@users.noreply.github.com>
2022-06-20 13:15:14 +0900

    updated Header!!

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./header.css";
+import { FiCoffee } from "react-icons/fi";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="container container--header">
+      <FiCoffee size="3rem" />
+      <h2>Tsuyama Cafe Generator</h2>
+    </div>
+  );
 };
 
 export default Header;
